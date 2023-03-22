3e04848212d64b0e27a61c353a549baacdc27e53
Sankethgadadinni <72505268+sankethgadadinni@users.noreply.github.com>
2023-03-22 16:10:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ def main():
     st.title("WebGPT")
     
     query_text = st.text_input("enter the query")
-    
-    if st.button('predict', use_container_width=True):
-    
-        results = webgpt_results1(query = query_text, number_of_results = 5)
-    
-        st.write(results)
+   
+    results = webgpt_results1(query = query_text, number_of_results = 5)
+
+    st.write(results)
     
 
 if __name__ == '__main__':
