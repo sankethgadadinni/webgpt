3165ee4f039c35f73b6eceb71321362f6c02e0b2
Sankethgadadinni <72505268+sankethgadadinni@users.noreply.github.com>
2023-03-22 16:09:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     
     query_text = st.text_input("enter the query")
     
-    if st.button('predict'):
+    if st.button('predict', use_container_width=True):
     
         results = webgpt_results1(query = query_text, number_of_results = 5)
     
